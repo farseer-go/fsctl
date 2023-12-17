5c487a00e1a7e4bcf1060bcc4595db5da2fbe253
steden <1470804@qq.com>
2023-12-18 00:32:33 +0800

    添加编译脚本

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -o ./fsctl.darwin.amd64 -ldflags
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o ./fsctl.linux.amd64 -ldflags="-w -s" .
 GOOS=linux GOARCH=386 CGO_ENABLED=0 go build -o ./fsctl.linux.386 -ldflags="-w -s" .
 #windows
-#GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -o ./fsctl.windows.amd64 -ldflags="-w -s" .
-#GOOS=windows GOARCH=386 CGO_ENABLED=0 go build -o ./fsctl.windows.386 -ldflags="-w -s" .
+GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -o ./fsctl.windows.amd64 -ldflags="-w -s" .
+GOOS=windows GOARCH=386 CGO_ENABLED=0 go build -o ./fsctl.windows.386 -ldflags="-w -s" .
