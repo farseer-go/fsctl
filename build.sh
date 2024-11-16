e405b7081be066d3de313cbc56c810076ad8d03d
steden <1470804@qq.com>
2024-11-17 02:08:54 +0800

    增加mac arm64的编译

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #mac
 GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -o ./fsctl.Darwin.x86_64 -ldflags="-w -s" .
+GOOS=darwin GOARCH=arm64 CGO_ENABLED=0 go build -o ./fsctl.Darwin.arm64 -ldflags="-w -s" .
+sudo \cp ./fsctl.Darwin.arm64 /usr/local/bin/fsctl
+
 #linux64
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o ./fsctl.Linux.x86_64 -ldflags="-w -s" .
 GOOS=linux GOARCH=386 CGO_ENABLED=0 go build -o ./fsctl.Linux.i686 -ldflags="-w -s" .
